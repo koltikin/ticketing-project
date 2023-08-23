b34bf9f87aa5185b3c9d53fdf7da7c3d31758044
ziya <koltikin210@gmail.com>
2023-08-23 19:27:12 +0300

    users list model created and some dummy users list sent to the user create UI

--- src/main/java/com/cydeo/controller/UserControl.java
+++ src/main/java/com/cydeo/controller/UserControl.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,11 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserControl {
 
     private final RoleService roleService;
+    private final UserService userService;
 
     @GetMapping("create")
     public String createUser(Model model){
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("userList",userService.findAll());
 
         return "/user/create";
     }
