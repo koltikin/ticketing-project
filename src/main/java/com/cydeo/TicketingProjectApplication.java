33a672daab249f7b32a1def86555675368c8a8ae
ziya <koltikin210@gmail.com>
2023-08-23 06:50:06 +0300

    files are renamed

--- src/main/java/com/cydeo/TicketingProjectApplication.java
+++ src/main/java/com/cydeo/TicketingProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketingProjectMvcApplication {
+public class TicketingProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TicketingProjectMvcApplication.class, args);
+        SpringApplication.run(TicketingProjectApplication.class, args);
     }
 
 }
